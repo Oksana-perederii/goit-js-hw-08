7f9045553239cef19098ac4ffb32bafcd85af596
Oksana-perederii <novamova.nemchenko@gmail.com>
2023-05-07 20:00:40 +0300

    make homework 08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,52 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-
-const imageList = galleryItems
-  .map(({ preview, original, description }) => {
-    return ` <li class="gallery__item">
-  <a class="gallery__link"
-   href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
- </li>`;
-  })
-  .join('');
-
-gallery.insertAdjacentHTML('afterbegin', imageList);
-
-gallery.addEventListener('click', onImageClick);
-
-function onImageClick(event) {
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  event.preventDefault();
-  const onCloseModal = event => {
-    const ESC_KEY = 'Escape';
-
-    if (event.code === ESC_KEY) {
-      instance.close();
-    }
-  };
-
-  const instance = basicLightbox.create(
-    `<img src = "${event.target.dataset.source}" width ="800"
-        height = "600">`,
-    {
-      onShow: instance => {
-        window.removeEventListener('keydown', onCloseModal);
-      },
-    }
-  );
-  instance.show();
+const galleryLightBoxMarkup = createLightBoxImgGallery(galleryItems);
+gallery.insertAdjacentHTML('beforeend', galleryLightBoxMarkup);
+function createLightBoxImgGallery() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+     `;
+    })
+    .join(``);
 }
+const galleryLightBox = new SimpleLightbox(`.gallery__link`, {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+console.log(galleryLightBox);
 console.log(galleryItems);
