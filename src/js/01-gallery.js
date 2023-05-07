74c74a62036290ebdc31e861e2ad2c20ea9368d7
Oksana-perederii <novamova.nemchenko@gmail.com>
2023-05-07 19:13:14 +0300

    rename homepage

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
+// Change code below this line
+const gallery = document.querySelector('.gallery');
+
+const imageList = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <li class="gallery__item">
+  <a class="gallery__link"
+   href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+ </li>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', imageList);
+
+gallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  event.preventDefault();
+  const onCloseModal = event => {
+    const ESC_KEY = 'Escape';
+
+    if (event.code === ESC_KEY) {
+      instance.close();
+    }
+  };
+
+  const instance = basicLightbox.create(
+    `<img src = "${event.target.dataset.source}" width ="800"
+        height = "600">`,
+    {
+      onShow: instance => {
+        window.removeEventListener('keydown', onCloseModal);
+      },
+    }
+  );
+  instance.show();
+}
 console.log(galleryItems);
